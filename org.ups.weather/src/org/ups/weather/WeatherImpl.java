76089ae05d0212f009ad441ae872186e2c1250ca
loclamor <favier.loic.31@gmail.com>
2013-11-15 16:44:39 +0100

    add IWeather implementation

---
+++ org.ups.weather/src/org/ups/weather/WeatherImpl.java
@@ -0,0 +1,25 @@
+package org.ups.weather;
+
+public class WeatherImpl implements IWeather {
+
+	public void addListener(IWeatherListener listener) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void removeListener(IWeatherListener listener) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public WeatherType getCurrentWeather() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public WeatherType getWeather(int nbHoursFromNow) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
